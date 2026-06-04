ed3593a969ceb3e51cf3668ed3fb4b64e6977056
Mangesh Pise <mppise@gmail.com>
2026-06-04 10:43:04 -0400

    Release v1.1.3

--- release.sh
+++ release.sh
@@ -136,7 +136,7 @@ sync_marketplace_from_plugin() {
       plugins: [
         {
           name: plugin.name,
-          source: 'https://github.com/specgantry/specgantry.github.io.git',
+          source: 'specgantry/specgantry.github.io',
           displayName: plugin.displayName,
           description: plugin.description,
           version: plugin.version,
